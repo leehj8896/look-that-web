3a7e4fdcc30d48c3bb5c65c68d19dcd9f1cf6a33
leehj8896 <leehj8896@gmail.com>
2024-08-10 16:11:16 +0900

    feat: 공통 API 유틸 get 메소드 querystring 추가

--- src/utils.ts
+++ src/utils.ts
@@ -5,14 +5,31 @@ export const fetchData = async (params: {
   path: string,
   method?: HTTP_METHOD,
   body?: BodyInit,
+  query?: Record<string, any>
 }) => {
   console.log('fetchData', 'params', params)
-  const { path, method, body } = params
+  let { path, method, body, query } = params
 
-  const response = await fetch(`${constants.API_URL}${path}`, {
-    method: method || HTTP_METHOD.GET,
-    body: body || null,
-  })
+  let response
+  let url = new URL(path, constants.API_URL)
+
+  if (!method) method = HTTP_METHOD.GET
+  switch (method) {
+    case HTTP_METHOD.GET:
+      url.search = new URLSearchParams(query).toString();
+      response = await fetch(url, {
+        method: HTTP_METHOD.GET,
+      })
+      break
+    case HTTP_METHOD.POST:
+      response = await fetch(url, {
+        method: HTTP_METHOD.POST,
+        body: body,
+      })
+      break
+  }
+  if (!response) return {}
+
   const jsonData = await response.json()
   console.log('response', jsonData)
   return jsonData
